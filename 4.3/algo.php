ccea30c0da19e3b40ec47e2dea5b270653e18368
Romain CROS <romaincros17@gmail.com>
2025-11-18 15:22:19 +0100

    exo 4.3

---
+++ 4.3/algo.php
@@ -0,0 +1,19 @@
+<?php
+$contacte = fopen("tables.txt", "r");
+$status = 1;
+while(!feof($contacte)){
+    if($status == 1){
+        $status++;
+        print_r(fgets($contacte));
+    }else{
+        $ligne = explode(" ", fgets($contacte));
+        for ($i = 0; $i<=count($ligne)-1 ; $i++){
+            if($i != 0 && $ligne[0]*$i !=$ligne[$i]){
+                print_r($ligne[0]*$i. " ");
+            }else{
+                print_r($ligne[$i]." ");
+            }}
+        echo "<br>";
+    }
+};
+?>
