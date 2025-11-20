08afe34f7cb3f4b7c02cffcb954f3b9f8234618d
Romain CROS <romaincros17@gmail.com>
2025-11-20 11:15:46 +0100

    update EXO Variable session 6.0 avec bouton supprimer

--- variaglessesion 6.0/index.php
+++ variaglessesion 6.0/index.php
@@ -14,6 +14,7 @@
 <button type="submit">valider</button>
 
 
+
 </form>
 </body>
 </html>
--- variaglessesion 6.0/traitement.php
+++ variaglessesion 6.0/traitement.php
@@ -3,5 +3,8 @@ session_start();
 $_SESSION["username"] = $_POST["username"];
 
 echo "<h1>Bonjour " . $_SESSION["username"] . "</h1>";
-
 ?>
+
+<a href="index.php">
+    <button onclick = "unset($_SESSION['username']);">supprimer</button>
+</a>
